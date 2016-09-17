3e4638dd1beeb47b5ad1092d0acb2ce588f6ab6a
daniel@danielthegeek.com <Dan@Dans-MacBook-Pro.local>
2016-09-17 17:35:38 -0400

    Changes

---
+++ Assets/Scripts/EarthManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EarthManager : MonoBehaviour {
+
+	public GameObject probe; //Remove This
+
+
+	private float y; 
+	public float earthRotationSpeed = 10.0f;  
+	public int clickerScore = 0;
+	private Vector2 myPos;
+
+	void Start () {
+		//Grabbing Earth's position so we can spawn the ships at the same location. 
+		myPos = new Vector2(transform.position.x, transform.position.y);
+	}
+
+	void OnMouseDown(){
+
+		clickerScore++;
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		y -= Time.deltaTime * earthRotationSpeed;
+		transform.rotation = Quaternion.Euler(0,y,0);
+	}
+
+	public void SpawnShips(GameObject type, Vector2 locationOfAstroid){
+
+		//Spawn the ship
+		GameObject ship = (GameObject)Instantiate (type, myPos, Quaternion.identity);
+
+		//Give the spawned ship the Vector2 location of the Astrod it's traveling to. 
+		ship.GetComponent<MoveProbe>().moveToLocation = locationOfAstroid; 
+
+
+	}
+
+	public void BringShipsBack(GameObject type, Vector2 locationOfAstroid){
+
+		//Spawn the ship
+		GameObject ship = (GameObject)Instantiate (type, locationOfAstroid, Quaternion.identity);
+
+		//Give the spawned ship the Vector2 location of the Astrod it's traveling to. 
+		ship.GetComponent<MoveProbe>().moveToLocation = myPos; 
+
+
+	}
+}
