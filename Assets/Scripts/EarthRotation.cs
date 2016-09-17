5a67df4f71ea490169fb508d32e855c07fb08d5d
daniel@danielthegeek.com <Dan@Dans-MacBook-Pro.local>
2016-09-17 14:01:20 -0400

    Changes

--- Assets/Scripts/EarthRotation.cs
+++ Assets/Scripts/EarthRotation.cs
@@ -3,25 +3,41 @@ using System.Collections;
 
 public class EarthRotation : MonoBehaviour {
 
-	public Transform probe;
-	private float y; 
-	public float speed = 10.0f; 
-	public int clickerScore = 0;
+	public GameObject probe; //Remove This
 
-	// Use this for initialization
+
+	private float y; 
+	public float earthRotationSpeed = 10.0f;  
+	public int clickerScore = 0;
+	private Vector2 myPos;
+
 	void Start () {
-	
+		//Grabbing Earth's position so we can spawn the ships at the same location. 
+		myPos = new Vector2(transform.position.x, transform.position.y);
 	}
 
-
 	void OnMouseDown(){
+		
 		clickerScore++;
-		Instantiate(probe, new Vector3(0, 0, 0), Quaternion.identity);
+
+		//Just sending a random location on mouse down for testing.  
+		SpawnShips (probe, new Vector2 (Random.Range(-500.0f, 500.0f), Random.Range(-500.0f, 500.0f)));
 	}
 
 	// Update is called once per frame
 	void Update () {
-		y += Time.deltaTime * speed;
+		y -= Time.deltaTime * earthRotationSpeed;
 		transform.rotation = Quaternion.Euler(0,y,0);
+	}
+
+	public void SpawnShips(GameObject type, Vector2 locationOfAstroid){
+
+		//Spawn the ship
+		GameObject ship = (GameObject)Instantiate (type, myPos, Quaternion.identity);
+
+		//Give the spawned ship the Vector2 location of the Astrod it's traveling to. 
+		ship.GetComponent<MoveProbe>().moveToLocation = locationOfAstroid; 
+
+
 	}
 }
--- Assets/Scripts/MoveProbe.cs
+++ Assets/Scripts/MoveProbe.cs
@@ -3,19 +3,26 @@ using System.Collections;
 
 public class MoveProbe : MonoBehaviour {
 	public float target = 2070.0F;
-	public float speed = 45.0F;
+	public float speed = 1.0F;
 
-	private Rigidbody2D rb;
+	public Vector2 moveToLocation;
+
 
 	// Use this for initialization
 	void Start () {
-		rb = gameObject.GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 			
-		rb.velocity = new Vector2(5, 5);
-	}
+		// set rotation to move Location
+		Quaternion rot = Quaternion.LookRotation(transform.position - new Vector3(moveToLocation.x, moveToLocation.y, transform.position.z) , Vector3.forward); 
 
+		transform.rotation = rot; //rotates game object to correct position
+		Debug.Log(transform.rotation);
+		transform.eulerAngles = new Vector3(0,0,transform.eulerAngles.z); //remove X and y rotation
+
+		transform.position = Vector2.MoveTowards(new Vector2(transform.position.x, transform.position.y), moveToLocation, speed * Time.deltaTime);
+	}
+		
 }
