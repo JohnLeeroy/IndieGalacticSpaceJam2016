b8f413059a27f57d252ae93198845460645137a0
John Li <jli@unikry.com>
2016-09-17 16:36:44 -0400

    Added solar collector.  Moved scripts into building.

--- Assets/Scripts/Model/Attributes/ICapacity.cs
+++ Assets/Scripts/Model/Attributes/ICapacity.cs
@@ -3,5 +3,5 @@ using System.Collections;
 
 public interface ICapacity {
 
-	float GetCapacity();
+	int getSize();
 }
---
+++ Assets/Scripts/Model/Unit/Building/SolarCollector.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SolarCollector : BaseBuilding {
+
+	public float powerCapacity = 100; 
+
+	override public int getUnitTypeId() {
+		return 837123; //random
+	}
+}
