3a7105b127a96fbf3463a4bce04b046649717187
daniel@danielthegeek.com <Dan@Dans-MacBook-Pro.local>
2016-09-17 11:29:31 -0400

    Changes

--- Assets/Scripts/EarthRotation.cs
+++ Assets/Scripts/EarthRotation.cs
@@ -3,15 +3,22 @@ using System.Collections;
 
 public class EarthRotation : MonoBehaviour {
 
-
+	public Transform probe;
 	private float y; 
 	public float speed = 10.0f; 
+	public int clickerScore = 0;
 
 	// Use this for initialization
 	void Start () {
 	
 	}
-	
+
+
+	void OnMouseDown(){
+		clickerScore++;
+		Instantiate(probe, new Vector3(0, 0, 0), Quaternion.identity);
+	}
+
 	// Update is called once per frame
 	void Update () {
 		y += Time.deltaTime * speed;
---
+++ Assets/Scripts/MoveProbe.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveProbe : MonoBehaviour {
+	public float target = 2070.0F;
+	public float speed = 45.0F;
+
+	private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			
+		rb.velocity = new Vector2(5, 5);
+	}
+
+}
